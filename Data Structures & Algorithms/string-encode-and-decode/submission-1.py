9c8509286ccb28e04353b71594332665f0c3db2d
Akhil Krishnamurthy <akhil.krishnamurthy2006@gmail.com>
2026-08-09 19:26:32 -0500

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # have a # to indicate the stop of the length
+        # before that have the number of characters that we want to iterate
+        # then iterate the amount of characters after the hash
+
+        res = []
+
+        for s in strs: 
+            res.append(f"{len(s)}#{s}")
+        return "".join(res)
+
+    def decode(self, s: str) -> List[str]:
+        i = 0
+        res = []
+        while i < len(s):
+            length = []
+            while s[i] != "#":
+                length.append(s[i])
+                i += 1
+            length = int("".join(length))
+            
+            temp = []
+            j = i + 1
+            while j < i + length + 1:
+                temp.append(s[j])
+                j += 1
+            res.append("".join(temp))
+            temp = []
+            i = j
+        
+        return res
+
+            
+            
